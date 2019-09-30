22f45d411ccbc5d963b4965841dff70c9db1a72e
Tusko <tusko@photoinside.me>
2019-09-30 12:26:29 +0300

    fix build

---
+++ express/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+const rs = require("./eventHandler");
+
+app.get("/", (req, res) => rs.processing(req, res));
+
+app.post("/", (req, res) => rs.processing(req, res));
+
+app.get("*", (req, res) => {
+  res.status(404).json(rs.response(true, "Page not found"));
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-const express = require("express");
-const serverless = require("serverless-http");
-const app = express();
-const rs = require("./eventHandler");
+const app = require("./express/app");
 const isDev = process.env.NODE_ENV === "development";
-
-app.get("/", (req, res) => rs.processing(req, res));
-
-app.post("/", (req, res) => rs.processing(req, res));
-
-app.get("*", (req, res) => {
-  res.status(404).json(rs.response(true, "Page not found"));
-});
 
 if (isDev) {
   const appPort = process.env.PORT || 3030;
@@ -18,5 +7,3 @@ if (isDev) {
     console.log("Listening on http://localhost:" + appPort);
   });
 }
-
-module.exports.handler = serverless(app);
