8697f055013b78b2f301eab9e45393dd10a7dae9
Tusko <tusko@photoinside.me>
2019-09-30 11:58:39 +0300

    added netlify lambda

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const serverless = require("serverless-http");
-const bodyParser = require("body-parser");
 const appPort = process.env.PORT || 3030;
 const app = express();
 const rs = require("./eventHandler");
-
-app.use(bodyParser);
+const isDev = process.env.NODE_ENV === "development";
 
 app.get("/", (req, res) => rs.processing(req, res));
+
+app.post("/", (req, res) => rs.processing(req, res));
 
 app.get("*", (req, res) => {
   res.status(404).json(rs.response(true, "Page not found"));
 });
 
-app.listen(appPort, () => {
-  console.log("Listening on http://localhost:" + appPort);
-});
+if (isDev) {
+  app.listen(appPort, () => {
+    console.log("Listening on http://localhost:" + appPort);
+  });
+}
 
 module.exports.handler = serverless(app);
